8a5e28ef6c48bc472364eedccfda6fb82273e6bb
khiromoshito <labanan02@gmail.com>
2021-03-01 20:51:46 +0800

    Docs: added loader

---
+++ scripts/loader.js
@@ -0,0 +1,24 @@
+PageRoute.onRoutingStart = (url) => {
+    let _route_loader = document.createElement("div");
+    _route_loader.id = "_route_loader";
+    _route_loader.setAttribute("style", `
+        background-color: crimson;
+        height: 3px;
+        width: 0%;
+        position: fixed;
+        top: 0;
+        left: 0;
+        transition: 50ms;
+        z-index: 2000;
+    `);
+    document.body.appendChild(_route_loader);
+}
+PageRoute.onRoutingProgress = (url, progress) => {
+    let _route_loader = document.querySelector("#_route_loader");
+    if(_route_loader) _route_loader.style.width = (progress*100) + "%";
+}
+PageRoute.onRoutingSuccess = (url) => {
+    let _route_loader = document.querySelector("#_route_loader");
+    if(_route_loader && _route_loader.parentElement) 
+        _route_loader.parentElement.removeChild(_route_loader);
+}
